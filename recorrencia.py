c689771a79c645a030b4ce52704aba1b54bcf984
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-10-30 08:30:08 -0300

    Verificação da entrada para ser numero entre 0 e 90

---
+++ recorrencia.py
@@ -0,0 +1,10 @@
+# Conversão para radianos
+#R = X * 3,1415
+
+X = int(input("Digite um valor entre 0 e 90: "))
+
+while X < 0 or X > 90:
+    print("Valor inválido. Precisa estar entre 0 e 90")
+    X = int(input("Digite um valor entre 0 e 90: "))
+
+# Não pode usar fatorial nem elevação
