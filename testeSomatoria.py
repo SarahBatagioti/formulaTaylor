95f61770bc397971a026b9fcc3ea9f2723b6fc5d
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-10-30 08:58:52 -0300

    Anotações da lógica

---
+++ testeSomatoria.py
@@ -0,0 +1,64 @@
+'''
+def somatorio(x):
+    if x==1:
+        return 1
+    else:
+        return x + somatorio(x-1)
+
+while True:
+    x = int(input("Somatorio de 1 até: "))
+    print("Soma: ",somatorio(x) )
+
+n = 5
+def som(n):
+    if n==1:
+        return 1
+    else:
+        return n + som(n-1)
+
+print (som(n))
+
+for (var i; i < 10; i++){
+    console.log(i);
+}
+
+i = 1 #inicializador
+while i < 10: #verifica a condição
+    print(i) #o corpo, a ação
+    i += 2 #o passo de "incremento"
+'''
+
+# lógica 
+
+def somatoria (função que é de 0 até 5)
+
+r = x * 3.1415/180
+x = input (o <= x <= 90)
+
+exressão1 = (-1) (valor 1 da def somatoria)
+exressão1 = (-1)*(-1) (valor 2 da def somatoria)
+exressão1 = (-1)*(-1)*(-1) (valor 3 da def somatoria) 
+exressão1 = (-1)*(-1)*(-1)*(-1) (valor 4 da def somatoria)
+exressão1 = (-1)*(-1)*(-1)*(-1)*(-1) (valor 5 da def somatoria)  
+
+expressao2 = 2  (valor 1 da def somatoria)
+expressao2 = 2 * 2 (valor 2 da def somatoria)
+expressao2 = 2 * 2 * 2 (valor 3 da def somatoria) 
+expressao2 = 2 * 2 * 2 * 2 (valor 4 da def somatoria)
+expressao2 = 2 * 2 * 2 * 2 * 2 (valor 5 da def somatoria)  
+
+expressao3 = r ^ expressao2
+
+expressao4 = (2 * (1)) (fatorial do valor 1 da def somatoria)
+expressao4 = ( 2 * (2 * 1)) (fatorial do valor 2 da def somatoria)
+expressao4 = ( 2 * (3 * 2 * 1)) (fatorial do valor 3 da def somatoria)
+expressao4 = ( 2 * (4 * 3 * 2 * 1)) (fatorial do valor 4 da def somatoria)
+expressao4 = ( 2 * (5 * 4* 3 * 2 * 1)) (fatorial do valor 5 da def somatoria)
+
+expressao5 = exressão1 * (expressao3/expressao4)
+
+ou seja, vai ficar no final:
+
+(-1)^0 * ( (r)^0 / (0)! ) + (-1)^1 * ( (r)^2 / (2)! ) + (-1)^2 * ( (r)^4 / (4)! ) + (-1)^3 * ( (r)^6 / (6!) ) + (-1)^4 * ( (r)^8 / (8)! ) + (-1)^5 * ( (r)^10 / (10! )
+
+
